9e233f57f13904a5128d138dce6b8d009a14d4c9
Juan Carlos Vidal <juancarvp@gmail.com>
2023-09-13 17:23:41 -0500

    First update

--- eclipse-workspace/lab03-4C24-GP01/src/main/java/com/tecsup/lab03/dto/SutendtDto.java
+++ eclipse-workspace/lab03-4C24-GP01/src/main/java/com/tecsup/lab03/dto/SutendtDto.java
@@ -3,6 +3,6 @@ package com.tecsup.lab03.dto;
 public class SutendtDto {
 	public static void main( String[] args )
     {
-        System.out.println( "Hello Dto!" );
+        System.out.println( "Hello World!" );
     }
 }
